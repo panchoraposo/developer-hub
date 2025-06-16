535313895e1f17abbb2da9f3804ad12f9bdf2463
Francisco Raposo <fraposo@redhat.com>
2025-06-16 11:29:38 -0400

    test

--- setenv.sh
+++
@@ -1,8 +0,0 @@
-oc project backstage
-
-oc set env deployment/backstage-developer-hub \
-        GITLAB_TOKEN=glpat-KmSSwDUmwxmzywGzs5p8 \
-        NODE_TLS_REJECT_UNAUTHORIZED=0 \
-        JENKINS_TOKEN=1164ae195198dd70d3c0ba67eace2d1b42 \
-        OAUTH_CLIENT_SECRET=XYNZOBNpd1o0LFONwyMZ15YmaTsZ0HQT \
-        OPENSHIFT_CLUSTER_INGRESS_DOMAIN=apps.cluster-tb8sp.tb8sp.sandbox1673.opentlc.com
