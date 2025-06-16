679337b657f38741d518d1c5c81000cbbb524134
Francisco Raposo <fraposo@redhat.com>
2025-06-16 11:27:37 -0400

    gitlab

--- keycloak.sh
+++ keycloak.sh
@@ -5,4 +5,4 @@ openssl req -subj '/CN=demo.keycloak.org/O=Demo Keycloak./C=US' -newkey rsa:2048
 oc -n keycloak create secret tls rhbk-tls-secret --cert ./apps/keycloak/overlays/dev/tls/certificate.pem --key ./apps/keycloak/overlays/dev/tls/key.pem
 
 # ENV OpenShift
-oc set env deployment/keycloak OPENSHIFT_CLUSTER_INGRESS_DOMAIN=apps.cluster-brl4w.brl4w.sandbox968.opentlc.com
+oc set env deployment/keycloak OPENSHIFT_CLUSTER_INGRESS_DOMAIN=apps.cluster-tb8sp.tb8sp.sandbox1673.opentlc.com
